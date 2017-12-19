fb685bbddd3e0cbd9b075286981063ed997b4c8e
Eduardo Khattab <eduardo.khattab@gympass.com>
2017-12-19 18:30:36 -0200

    Jupyter Files

---
+++ modules/modulo.py
@@ -0,0 +1,8 @@
+def area_quadrado(lado):
+	return lado ** 2
+
+def area_retangulo(base, altura):
+	return base * altura
+
+def perimetro_retangulo(base, altura):
+	return 2*base + 2*altura
---
+++ testando_modulo.py
@@ -0,0 +1,9 @@
+import sys 
+sys.path.insert(0, 'modules/modulo')
+import modulo
+
+print(modulo.area_quadrado(5))
+
+print(modulo.area_retangulo(4,2))
+
+print(modulo.perimetro_retangulo(4,2))
