d55a32c4eb78087e603b70b8e5ae9049e3d6327b
Eduardo Khattab <eduardo.khattab@gympass.com>
2017-12-29 16:00:39 -0200

    POO and functional

---
+++ testando_funcional.py
@@ -0,0 +1,11 @@
+dobro = lambda x: x*2
+print(dobro(3))
+
+soma = lambda x,y: x + y
+print(soma(10, 2))
+
+lista = [1,2,3,4,5,6,7,8,9,10]
+filter(lambda x: x % 2 == 0, lista)
+print(list(filter(lambda x: x % 2 == 0, lista)))
+
+print(list(map(lambda x: x * 2, lista)))
